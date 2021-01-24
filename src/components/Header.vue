97faf1f1a352d2ee1822c01a3e9bada079a506fd
web-2017 <webdevelope2017@gmail.com>
2021-01-24 21:59:04 +0300

    lesson 4

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,20 @@
 <template>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<nav class="navbar navbar-expand-lg navbar-light p-0 ">
 		<div class="container">
-			<a class="navbar-brand" href="#">Navbar</a>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-					<li class="nav-item" v-for="(menu, index) in headerMenuList" :key="index">
+			<a class="navbar-brand logo" href="/"><img src="../assets/images/icons/logo.png" alt="logo" /></a>
+			<div class="collapse collapse-fontsize navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto mb-lg-0">
+					<li class="nav-item menu-link" v-for="(menu, index) in headerMenuList" :key="index">
 						<router-link class="nav-link active" aria-current="page" :to="menu.path">
 							{{ menu.name }}
 						</router-link>
 					</li>
 				</ul>
-				<div>
-					<a href="#"><img src="../assets/images/icons/f.png" alt="" /></a>
-					<a href="#"><img src="../assets/images/icons/p.png" alt="" /></a>
-					<a href="#"><img src="../assets/images/icons/insta.png" alt="" /></a>
-					<a href="#"><img src="../assets/images/icons/g+.png" alt="" /></a>
+				<div class="icons">
+					<a href="https://www.facebook.com/" target="_blank"><img src="../assets/images/icons/f.png" alt="" /></a>
+					<a href="https://www.pinterest.ru/" target="_blank"><img src="../assets/images/icons/p.png" alt="" /></a>
+					<a href="https://www.instagram.com/" target="_blank"><img src="../assets/images/icons/insta.png" alt="" /></a>
+					<a href="https://myaccount.google.com/" target="_blank"><img src="../assets/images/icons/g+.png" alt="" /></a>
 				</div>
 			</div>
 		</div>
@@ -40,3 +40,38 @@
 		},
 	}
 </script>
+
+<style lang="scss">
+	@import '../assets/layouts/index';
+	.navbar {
+		height: 90px;
+	}
+	.navbar-nav {
+		height: 90px;
+	}
+	.logo {
+		padding-right: 180px;
+	}
+	.collapse-fontsize {
+		font-size: 18px;
+	}
+	.menu-link {
+		padding: 25px 30px;
+
+		&:hover {
+			background: $grey;
+		}
+	}
+
+	.icons {
+		a {
+			width: 30px;
+			height: 30px;
+			text-align: center;
+			border: 1px solid $grey;
+			border-radius: 50%;
+			margin-right: 10px;
+			display: inline-block;
+		}
+	}
+</style>
