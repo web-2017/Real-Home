330711aa73ee13d39907529c0472dbe7542ac099
web-2017 <webdevelope2017@gmail.com>
2021-01-28 00:24:51 +0300

    lesson-5 Footer layouts

---
+++ src/components/Footer/FooterContacts.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="col">
+		<h3 class="footer__title">Contact us</h3>
+		<ul class="footer__list footer__list_capitalize">
+			<li>
+				<p>240 Central Park, London OR 10019</p>
+			</li>
+			<li>
+				<p>Freephone: +1 800 559 6580</p>
+				<p>Telephone: +1 959 603 6035</p>
+				<p>FAX: +1 8</p>
+			</li>
+			<li>
+				<a class="footer__link footer__link_red footer__link_underline" href="mailto:info@realhome.com" target="_blank">
+					info@realhome.com
+				</a>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			footerMenuClient: {
+				type: Array,
+				required: true,
+			},
+		},
+	}
+</script>
---
+++ src/constants/menuLinks.js
@@ -0,0 +1,14 @@
+export const menuList = [
+	{ name: 'home', path: '/' },
+	{ name: 'About Us', path: '/about' },
+	{ name: 'Property', path: '/property' },
+	{ name: 'Our Blog', path: '/blog' },
+	{ name: 'Contacts', path: '/contacts' },
+]
+
+export const menuClient = [
+	{ name: 'Sign in', path: 'Sign' },
+	{ name: 'Forum', path: '/forum' },
+	{ name: 'Promotions', path: '/promotions' },
+	{ name: 'News', path: '/news' },
+]
